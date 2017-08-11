d66968917849acc39a92f93558b594b58bece65e
cmsun <351721714@qq.com>
2017-08-11 11:02:59 +0800

    更正strncpy

--- strcpy.c
+++ strcpy.c
@@ -7,7 +7,7 @@
 *  @FileName       : strcpy.c
 *  @Author         : scm 351721714@qq.com
 *  @Create         : 2017/06/05 20:01:26
-*  @Last Modified  : 2017/07/13 19:50:23
+*  @Last Modified  : 2017/08/11 10:59:28
 ********************************************************************************
 */
 
@@ -27,13 +27,16 @@ char *strcpy(char *dst, const char *src)
 
 char *strncpy(char *dst, int n, const char *src)
 {
-    char *temp = dst;
     if(dst == NULL || src == NULL || n <= 1)
         return NULL;
     if(dst == src)
         return dst;
-    while((*dst++ = *src++) != '\0' && n--)
-        ;
+    char *temp = dst;
+    int cpylen = 0;
+    while(cpylen < n && (*dst++ = *src++) != '\0')
+        ++cpylen;
+    if(cpylen == n)
+        dst[n - 1] = '\0';
     return temp;
 }
 
