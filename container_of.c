51f3ea98f698cde0cbacfb67d71db788a03031d5
cmsun <351721714@qq.com>
2017-09-08 00:03:07 +0800

    增加memcpy.c更正bitmove.c

--- container_of.c
+++ container_of.c
@@ -7,7 +7,7 @@
 *  @FileName       : container_of.c
 *  @Author         : scm 351721714@qq.com
 *  @Create         : 2017/08/25 14:03:59
-*  @Last Modified  : 2017/08/26 00:37:42
+*  @Last Modified  : 2017/09/07 10:44:33
 ********************************************************************************
 */
 
@@ -15,7 +15,7 @@
 
 //如果知道知道结构体类型，以及结构体某个成员的指针，求成员指针所在结构体实例的内存首地址。
 
-#define container_of(ptr, type, member) ((void *)ptr - (size_t)&(((type *)0)->member))
+#define container_of(ptr, type, member) ((char *)ptr - (unsigned long)&(((type *)0)->member))
 
 typedef struct ContainerTest
 {
---
+++ memcpy.c
@@ -0,0 +1,61 @@
+/*
+********************************************************************************
+*                              COPYRIGHT NOTICE
+*                             Copyright (c) 2016
+*                             All rights reserved
+*
+*  @FileName       : memcpy.c
+*  @Author         : scm 351721714@qq.com
+*  @Create         : 2017/09/06 17:52:08
+*  @Last Modified  : 2017/09/07 23:49:58
+********************************************************************************
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+//必须考虑内存重叠的情况和超出内存范围的情况。
+
+//假设单片机的RAM地址从0x20000000开始，总共64K
+#define MEM_SIZE 64 //64K
+#define MEM_START 0x20000000UL
+#define MEM_END (MEM_START + MEM_SIZE * 1024)
+
+#define is_overload(dst_, src_, len_) \
+        (dst_ < (void *)MEM_START || src_ < (void *)MEM_START || \
+        (dst_ + len_) >= (void *)MEM_END || (src_ + len_) >= (void *)MEM_END)
+
+void *my_memcpy(void *dst, void *src, size_t len)
+{
+    if(dst == NULL || src == NULL || dst == src || len <= 0)
+        return NULL;
+    //检测地址是否合法，测试时应该将它注销
+    /*
+     * if(is_overload(dst, src, len))
+     *     return NULL;
+     */
+    char *d = (char *)dst, *s = (char *)src;
+    if(s < d && s + len > d) //内存重叠，且源地址比目标地址小。
+    {
+        d += len - 1;
+        s += len - 1;
+        while(len--)
+            *d-- = *s--;
+    }
+    else                    //内存不重叠，或者重叠但源地址比目标地址大。
+    {
+        while(len--)
+            *d++ = *s++;
+    }
+    return dst;
+}
+
+int main(int argc, char *argv[])
+{
+    char s1[100] = "abcdefghigk";
+    char *s2 = s1+5;
+    printf("%s\n", my_memcpy(s2, s1, strlen(s1) + 1));
+    printf("s1: %s\n", s1);
+    printf("s2: %s\n", s2);
+    return 0;
+}
